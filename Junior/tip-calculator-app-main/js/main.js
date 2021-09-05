d09a532fde1112339f114dfdf1263f418e0cdb7a
leob4rr0s <cristianleonardobm@gmail.com>
2021-09-05 11:46:24 -0300

    html n css calculator finished

---
+++ Junior/tip-calculator-app-main/js/main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+$(document).ready( function () {
+    var total = $('#billInput').val()
+    //$('#amount-result').html(total)
+    console.log(total)
+});
+
+const billInput = document.getElementById("billInput");
+const cantPeople = document.getElementById("cantPeople");
+const btnFive = document.getElementById("five");
+const btnTen = document.getElementById("ten");
+const btnFifteen = document.getElementById("fiftenn");
+const btnTwentyfive = document.getElementById("twentyfive");
+const btnFifty = document.getElementById("fifty");
+const customTip = document.getElementById("customTip");
+
+//function myFunction(){
+//    console.log(billInput.value + "detecta")
+//}
+
