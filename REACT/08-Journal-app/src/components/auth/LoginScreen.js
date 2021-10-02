2c2635bb5ec9e0ca88a37fca7497fb8d364c8a58
leob4rr0s <cristianleonardobm@gmail.com>
2021-10-01 23:58:52 -0300

    desarrollo de journalapp

--- REACT/08-Journal-app/src/components/auth/LoginScreen.js
+++ REACT/08-Journal-app/src/components/auth/LoginScreen.js
@@ -1,18 +1,40 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { login } from '../../actions/auth'
+import { useForm } from '../../hooks/useForm'
 
 export const LoginScreen = () => {
+
+    const dispatch = useDispatch()
+
+    const [ formValues, handleInputChange ] = useForm( {
+        email: 'leobm@gmail.com',
+        password: '123456'
+    })
+
+    const { email, password} = formValues
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+        dispatch( login(12345, 'Leonardo') )
+        
+    }
+
+
     return (
         <>
             <h3 className="auth__title mb-5">Login</h3>
 
-            <form>
+            <form onSubmit= { handleLogin }>
                 <input
                     type="text"
                     placeholder="Email"
                     name="email"
                     className="auth__input"
                     autoComplete="off"
+                    value = {email}
+                    onChange= {handleInputChange}
                 />
 
                 <input
@@ -20,6 +42,8 @@ export const LoginScreen = () => {
                     placeholder="Password"
                     name="password"
                     className="auth__input"
+                    value = {password}
+                    onChange= {handleInputChange}
                 />
 
                 <button
--- REACT/08-Journal-app/src/components/notes/NoteScreen.js
+++ REACT/08-Journal-app/src/components/notes/NoteScreen.js
@@ -6,6 +6,31 @@ export const NoteScreen = () => {
         <div className="notes__main-contet">
             
             <NotesAppBar />
+
+            <div className="notes__content">
+
+                <input
+                    type="text"
+                    placeholder="Some awesome title"
+                    className="notes__title-input"
+                    autoComplete="off"
+                />
+
+                <textarea
+                    placeholder="What happened today"
+                    className="notes__textarea"
+                >
+                    
+                </textarea>
+
+                <div className="notes__image">
+                    <img 
+                    alt="lago canoas"
+                    src="https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80"></img>
+                    
+                </div>
+            </div>
+
         </div>
     )
 }
