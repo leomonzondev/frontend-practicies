5004c9dc6730e80a9f6988e1262c93fbf92d927c
leob4rr0s <cristianleonardobm@gmail.com>
2021-11-01 23:18:13 -0300

    mern memories-app

---
+++ MERN/Memories-App/client/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+
+
+import memories from './images/memories.png'
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import useStyles from './styles'
+
+const App = () => {
+    const classes = useStyles()
+
+    return (
+        <Container maxWidth="lg">
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading}  variant="h2" align="center">Memories</Typography>
+                <img className={classes.image}  src={memories} alt="memories" height="60"/>
+            </AppBar>
+
+            <Grow in>
+                <Grid container justify="space-between" alignItems="strech" spacing={3}>
+                    <Grid item xs={12} sm={7}>
+                        <Posts />
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Form />
+                    </Grid>
+                </Grid>
+            </Grow>
+
+        </Container>
+    )
+}
+
+export default App
