5f952ce9bd45befc57531472d51ac8c532a88e5a
leob4rr0s <cristianleonardobm@gmail.com>
2021-12-09 23:32:46 -0300

    a lot of CTF n start my portfolio

---
+++ miportfolioreact/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Hero } from './components/Hero'
+
+export const App = () => {
+    return (
+        <>
+            <Hero />
+        </>
+    )
+}
---
+++ miportfolioreact/src/components/Hero.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import githubimg from '../images/github.svg'
+import linkedingimg from '../images/linkedin.svg'
+import ytimg from '../images/youtube.svg'
+
+export const Hero = () => {
+    return (
+        <div className="hero">
+            <div className="title__hero">
+                <h1>Conecto ideas para los usuarios del mundo</h1>
+                
+                <p>Procurando la mejor calidad y experiencia digital</p>
+            </div>
+
+            <div className="footer__hero">
+                <ul>
+                
+                    <li> <img src={githubimg} alt="a"/></li>
+                    <li><img src={linkedingimg} alt=""/></li>
+                    <li><img  src={ytimg} alt=""/></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
