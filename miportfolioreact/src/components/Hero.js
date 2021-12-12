fd9f2d3b70b7a70d4695a32e91b9703051ed9bdd
leob4rr0s <cristianleonardobm@gmail.com>
2021-12-11 23:44:27 -0300

    Portfoliowork

--- miportfolioreact/src/components/Hero.js
+++ miportfolioreact/src/components/Hero.js
@@ -2,10 +2,16 @@ import React from 'react'
 import githubimg from '../images/github.svg'
 import linkedingimg from '../images/linkedin.svg'
 import ytimg from '../images/youtube.svg'
+import twimg from '../images/twitter.svg'
+import vid from '../video/video5.mp4'
+
 
 export const Hero = () => {
     return (
         <div className="hero">
+        <div className='cover-container'>
+            <video className="videoBG" src={vid} autoPlay={true} loop muted/>
+        </div>
             <nav>
                 <ul>
                     <li>Blog</li>
@@ -14,7 +20,7 @@ export const Hero = () => {
                 </ul>
             </nav>
             <div className="title__hero">
-                <h1>Conecto ideas para los usuarios del mundo</h1>
+                <h1>Conecto <span>ideas</span> para los usuarios del mundo</h1>
                 
                 <p>Procurando la mejor calidad y experiencia digital</p>
             </div>
@@ -22,9 +28,11 @@ export const Hero = () => {
             <div className="footer__hero">
                 <ul>
                 
-                    <li> <img src={githubimg} alt="a"/></li>
-                    <li><img src={linkedingimg} alt=""/></li>
-                    <li><img  src={ytimg} alt=""/></li>
+                    <li><a href="https://github.com/Leob4rr0s" target="_blank" rel="noopener noreferrer"> <img src={githubimg} alt="githubicon" /></a></li>
+                    <li><a href="https://www.linkedin.com/in/cristian-leonardo-barros-monzon" target="_blank" rel="noopener noreferrer"><img src={linkedingimg} alt="linkedinicon" /></a></li>
+                    <li><img  src={ytimg} alt="yticon" /></li>
+                    <li><a href="https://twitter.com/crisleobm" target="_blank" rel="noopener noreferrer"><img src={twimg} alt="twicon" /></a></li>
+                    
                 </ul>
             </div>
         </div>
