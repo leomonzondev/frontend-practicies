d460d2ac30a49d77b6747cd1cd2ef3fc51b8b09e
Leo monzon <cristianleonardobm@gmail.com>
2022-01-21 22:51:13 -0300

    useReducer

--- REACT/hooks-practica/src/index.js
+++ REACT/hooks-practica/src/index.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { CallbackHook } from './Components/06-memos/CallbackHook';
-import { Padre } from './Components/07-tarea-memo/Padre';
-// import { FormWithCustomHook } from './Components/02-useEffect/FormWithCustomHook';
-// import { App } from './App';
-// import { CounterApp } from './Components/01-useState/CounterApp';
-// import { CounterWithCustom } from './Components/01-useState/CounterWithCustom';
-// import { SimpleForm } from './Components/02-useEffect/SimpleForm';
-// import { MultipleCustomHooks } from './Components/03-examples/MultipleCustomHooks';
-// import { FocusScreen } from './Components/04-useRef/FocusScreen';
-// import { RealExample } from './Components/04-useRef/RealExample';
-// import { Layout } from './Components/05-useLayoutEffect/Layout.js';
-// import { Memorize } from './Components/06-memos/Memorize';
-// import { MemoHook } from './Components/06-memos/MemoHook';
-import './index.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { CallbackHook } from './Components/06-memos/CallbackHook';
+// import { Padre } from './Components/07-tarea-memo/Padre';
+// // import { FormWithCustomHook } from './Components/02-useEffect/FormWithCustomHook';
+// // import { App } from './App';
+// // import { CounterApp } from './Components/01-useState/CounterApp';
+// // import { CounterWithCustom } from './Components/01-useState/CounterWithCustom';
+// // import { SimpleForm } from './Components/02-useEffect/SimpleForm';
+// // import { MultipleCustomHooks } from './Components/03-examples/MultipleCustomHooks';
+// // import { FocusScreen } from './Components/04-useRef/FocusScreen';
+// // import { RealExample } from './Components/04-useRef/RealExample';
+// // import { Layout } from './Components/05-useLayoutEffect/Layout.js';
+// // import { Memorize } from './Components/06-memos/Memorize';
+// // import { MemoHook } from './Components/06-memos/MemoHook';
+// import './index.css';
 
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Padre />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Padre />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 
+import './Components/08-useReducer/intro-reducer'
