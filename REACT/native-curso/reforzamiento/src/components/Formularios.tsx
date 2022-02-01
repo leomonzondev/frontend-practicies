b17f58c334aa6fc9b33d9b36db95b3f91eb957fa
Leo monzon <cristianleonardobm@gmail.com>
2022-01-31 23:24:29 -0300

    fix: bug with java that didn't let me to run the app and started the development

--- REACT/native-curso/reforzamiento/src/components/Formularios.tsx
+++ REACT/native-curso/reforzamiento/src/components/Formularios.tsx
@@ -1,30 +1,21 @@
 import { useState } from "react";
+import { useForm } from '../Hooks/useForm';
 
 
 export const Formularios = () => {
 
-
-    const [formulario, setFormulario] = useState({
+    const { formulario, email, password,onChange } = useForm({
         email: 'test@test.com',
         password: '123456'
-    });
+    })
 
 
-    const onChange = ( value: string, campo: string) => {
-
-        setFormulario({
-            ...formulario,
-            [campo]: value
-            
-        })
-
-    }
 
     return (
     <>
         <h3>Formularios</h3>
-        <input type="text" onChange={({target}) => onChange( target.value, 'email')} value={formulario.email} className="form-control" placeholder="Email" />
-        <input type="text" onChange={({target}) => onChange( target.value, 'password')} value={formulario.password} className="form-control mt-2 mb-2" placeholder="Password" />
+        <input type="text" onChange={({target}) => onChange( target.value, 'email')} value={email} className="form-control" placeholder="Email" />
+        <input type="text" onChange={({target}) => onChange( target.value, 'password')} value={password} className="form-control mt-2 mb-2" placeholder="Password" />
     
     <code>
         <pre>{JSON.stringify ( formulario, null, 2)}</pre>
