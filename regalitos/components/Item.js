62f2cb4edac28d5e02a1b7e0c3d7be005db31e60
Leo monzon <cristianleonardobm@gmail.com>
2022-02-27 23:43:34 -0300

    feat: project drag images

--- regalitos/components/Item.js
+++ regalitos/components/Item.js
@@ -1,14 +1,48 @@
 import Image from 'next/image'
-import React, { useEffect, useReducer, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useReducer, useRef, useState } from 'react'
 
 import iconImg from '../images/icon-img.jpg'
 
 
 import { ImBin } from 'react-icons/im'
 import { itemReducer } from '../Reducer/itemReducer'
+import Dropzone, { useDropzone } from 'react-dropzone'
+import { BsCheckLg } from 'react-icons/bs'
 
 
 export const Item = ({ id, title, img, desc, bought}) => {
+
+
+    // const onDrop = useCallback( acceptedFiles => {
+
+    // },[])
+
+
+    // const { getRootProps, getInputProps, isDragActive } = useDropzone({onDrop})
+
+
+    const onDrop = useCallback((acceptedFiles) => {
+        acceptedFiles.forEach((file) => {
+          const reader = new FileReader()
+    
+          reader.onabort = () => console.log('file reading was aborted')
+          reader.onerror = () => console.log('file reading has failed')
+          reader.onload = () => {
+          // Do whatever you want with the file contents
+            const binaryStr = reader.result
+            console.log(binaryStr)
+          }
+          reader.readAsArrayBuffer(file)
+        })
+        
+      }, [])
+      const {getRootProps, getInputProps} = useDropzone({onDrop})
+    
+
+
+
+
+
 
     const [edit, setEdit] = useState(false)
 
@@ -85,7 +119,19 @@ export const Item = ({ id, title, img, desc, bought}) => {
 
   return (
     <div className='item' key={id}>
-        <Image className='img-item' src={iconImg} width='64px' height='64px' />
+        {/* <Image className='img-item' src={iconImg} width='64px' height='64px' /> */}
+        
+            <Dropzone onDrop={acceptedFiles => (<div>{acceptedFiles}</div>)}>
+    {({getRootProps, getInputProps}) => (
+        <section>
+        <div {...getRootProps()}>
+            <input style={{backgroundColor:'red', height:150, width:150}} placeholder='HERE' {...getInputProps()} />
+            <p style={{backgroundColor:'orange'}}>Drag 'n' drop some files here, or click to select files</p>
+        </div>
+        </section>
+    )}
+    </Dropzone>
+
         {
             edit && (<form onSubmit={(e) => handleSubmit(e)}><input ref={inputRef} autoFocus name='editInput' value={titleValue} onChange={handleInputChange} placeholder='Regalo...' /></form>) || (<div onDoubleClick={handleEdit}><h3>{titleValue}</h3></div>)
         }
