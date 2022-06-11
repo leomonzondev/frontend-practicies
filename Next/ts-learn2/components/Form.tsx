be9fa21c0b6b6ee440f6f423dc0d28aba50fa72a
Leo monzon <cristianleonardobm@gmail.com>
2022-06-10 22:45:56 -0300

    feat: todo ts

---
+++ Next/ts-learn2/components/Form.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Form = () => {
+  return (
+    <div className='flex items-center flex-col'>
+        <h1 className='text-2xl font-medium'>TODAY</h1>
+        <p>ASDASDASD</p>
+    </div>
+  )
+}
---
+++ Next/ts-learn2/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
